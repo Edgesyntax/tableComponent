8506fe2636110bca37a19a6ba6e194d5faf1f9e7
Kwame Adjei <rred2020@gmail.com>
2017-03-09 21:11:08 -0600

    update stories

--- stories/default.jsx
+++ stories/default.jsx
@@ -1,65 +1,10 @@
 // React Modules
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-// global.Perf = require('react-addons-perf');
+import React from "react";
 
 // Application Modules
-import Table, {Tr, Td, tableStylesheet} from "../dist/table.js";
-import tableMock from "./table.mock.yaml";
+import Table from "../dist/table.js";
+import minTableMock from "./minTable.mock.yaml";
 
-class Default extends Component{
-  renderTableList(){
-    return tableMock.map((item, index) => {
-      return (<Tr key={index}>
-        <Td column="id">{item._id}</Td>
-        <Td column="balance">{item.balance}</Td>
-        <Td column="age">{item.age}</Td>
-        <Td column="eyeColor">{item.eyeColor}</Td>
-        <Td column="name">{item.name}</Td>
-        <Td column="gender">{item.gender}</Td>
-        <Td column="company">{item.company}</Td>
-        <Td column="email">{item.email}</Td>
-        <Td column="registered">{item.registered}</Td>
-        <Td column="status">{item.isActive ? "Active" : "Inactive"}</Td>
-        <Td column="action">
-          <div style={{textAlign: "center"}}>
-            <button>
-            button
-              <i className="ion-ios-create-outline" style={tableStylesheet.i}></i>
-            </button>
-            <button
-              onClick={this.deleteItemHandler}
-              value={item._id}>
-              button
-              <i className="ion-ios-trash-outline" style={tableStylesheet.i}></i>
-            </button>
-          </div>
-        </Td>
-      </Tr>
-    )
-    });
-  }
-  render(){
-    return <Table
-      limit={25}
-      columns={[
-        {id: "id"},
-        {id: "balance"},
-        {id: "age"},
-        {id: "eyeColor"},
-        {id: "name"},
-        {id: "gender"},
-        {id: "company"},
-        {id: "email"},
-        {id: "action"}
-      ]}
-      showIndex={true}
-      sort={{
-        column: 1,
-        direction: "ASC" //DES // INI
-      }}
-      activeRow={{id: "id", value: "5844822b0604c338bfdbe2a7"}}>{this.renderTableList()}</Table>
-  }
-}
+const Default = (props) => <Table data={minTableMock} {...props}></Table>
 
 export default Default;
--- stories/index.js
+++ stories/index.js
@@ -5,4 +5,36 @@ import Default from "./default.jsx";
 
 storiesOf('Welcome', module)
   .add("Default", () => <Default />)
-  // .add("Custom Columns", () => <CustomColumns />);
+
+storiesOf("Columns", module)
+  .add("Rendered Columns", () => <Default columns={[{id: "balance"},{id: "email"},{id: "age"}]}/>)
+  .add("Custom Lables", () => <Default
+    columns={[
+      {id: "balance", label: "Remaining Balance"},
+      {id: "email", label: "Company email"},
+      {id: "age", label: "Current Age"}
+    ]}/>)
+
+storiesOf("Filtering", module)
+  .add("Filterable", () => <Default filterable={[]} />) // TODO: Add filterable
+  .add("Initial Filter", () => <Default filter="Brittany" />)
+  .add("Hide Filter", () => <Default hideFilter={true} />)
+
+storiesOf("Sorting", module)
+  .add("Sortable", () => <Default sortable={["name","balance"]}/>)
+  .add("Initial Sort", () => <Default sort={{column: 1, direction: "DES"}}/>)
+
+storiesOf("Limiting", module)
+  .add("Initial Limit", () => <Default limit={25}/>)
+  // .add("Hide Limit", () => <Default hideFilter={true} />) TODO: Add hide limit
+storiesOf("Custom Table", module)
+  .add("Render Table", () => <Default/>)
+storiesOf("All Options", module)
+  .add("Show index", () => <Default showIndex={true}/>)
+  .add("Active Row", () => <Default showIndex={true} activeRow={{id: "_id", value: "5844822b3e9c1401b8db0871"}}/>)
+// storiesOf("Pagination", module)
+// storiesOf("Events", module)
+
+
+// .add("Custom Columns", () => <CustomColumns />);
+// noDataText="No matching records found." TODO: Empty datasets
